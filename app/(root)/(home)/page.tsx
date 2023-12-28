cd8ccc8a6940df9101a4f4164df867a2f0b9408b
Angel Javier Carrion Torres <angeljavier@Angels-MBP.lan>
2023-12-27 21:57:07 -0400

    integration of mongodb and connection to front-end

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,40 +5,13 @@ import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import { getQuestions } from "@/lib/actions/question.actions";
 import Link from "next/link";
 
-const questions = [
-  {
-    _id: '1',
-    title: "Cascading Deletes in SQLAlchemy?",
-    tags: [{_id: '1', name: 'python'}, {_id: '2', name: 'sql'}],
-    author: {
-      _id: '1',
-      name: 'John Doe',
-      picture: 'john-doe.jpg'
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date('2021-08-20T09:53:31.000Z'),
-  },
-  {
-    _id: '2',
-    title: "How to center a div?",
-    tags: [{_id: '1', name: 'css'}, {_id: '2', name: 'sql'}],
-    author: {
-      _id: '1',
-      name: 'John Doe',
-      picture: 'john-doe.jpg'
-    },
-    upvotes: 10,
-    views: 100,
-    answers: [],
-    createdAt: new Date('2021-08-20T09:53:31.000Z'),
-  },
-]
+export default async function Home() {
 
-export default function Home() {
+  const result = await getQuestions({})
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -70,8 +43,8 @@ export default function Home() {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ?
-          questions.map((question) => (
+        {result.questions.length > 0 ?
+          result.questions.map((question) => (
           <QuestionCard
             key={question._id}
             _id={question._id}
---
+++ database/user.model.ts
@@ -0,0 +1,69 @@
+import { Schema, models, model, Document } from 'mongoose'
+
+export interface IUser extends Document {
+    clerkId: string;
+    name: string;
+    email: string;
+    username: string;
+    password?: string;
+    bio?: string;
+    picture: string;
+    location?: string;
+    portfolioWebsite?: string;
+    reputation?: number;
+    saved: Schema.Types.ObjectId[];
+    joinedAt: Date;
+}
+
+const UserSchema = new Schema({
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    bio: {
+        type: String,
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+    },
+    portfolioWebsite: {
+        type: String,
+    },
+    reputation: {
+        type: Number,
+    },
+    saved: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Question'
+    }],
+    joinedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const User = models.User || model('User', UserSchema);
+
+export default User
